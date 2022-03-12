a195aca55dfbc60cdc7749d9dd24232156880b91
Brooke Wilder <brookewilder19@gmail.com>
2022-03-12 13:26:32 -0500

    single view looks decent

--- src/screens/SingleRing/SingleRing.js
+++ src/screens/SingleRing/SingleRing.js
@@ -27,14 +27,19 @@ class SingleRing extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <ImageBackground
-          source={require("../../../assets/tree.png")}
-          style={{ opacity: 0.6, width: "100%", height: "100%" }}
-        >
-          <Text>{this.props.ring.quiz.value}</Text>
-        </ImageBackground>
-      </View>
+      <ImageBackground
+        source={require("../../../assets/tree.png")}
+        style={{ opacity: 0.6, width: "100%", height: "100%" }}
+      >
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.quizContainer} disabled='true'>
+            <Image source={{uri:this.props.ring.quiz.image}} style={{ flex:1, height:'100%', width:'100%'}}/>
+            <Text style={styles.value}>{this.props.ring.quiz.value}</Text>
+            <Text style={styles.name}>{this.props.ring.quiz.name}</Text>
+            <Text style={styles.description}>{this.props.ring.quiz.description}</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
     );
   }
 }
--- src/screens/SingleRing/styles.js
+++ src/screens/SingleRing/styles.js
@@ -1,10 +1,44 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-container:{
-    flex:1,
-    alignItems:'center',
-    marginTop:80,
-},
-})
+
+  container: {
+      flex:1,
+        justifyContent: 'center', 
+        alignContent: 'center',
+        alignItems: 'center'
+
+  },
+  quizContainer: {
+    alignSelf: 'center',
+    alignItems: "center",
+    backgroundColor: "white",
+    padding: 30,
+    height: 400,
+    width: 300,
+    margin: 30,
+    borderRadius:30,
+  },
+
+  value:{
+    fontFamily:'Arvo',
+    marginTop:20,
+    fontSize: 20,
+  },
+
+  name:{
+    fontFamily:'Oswald',
+    marginTop: 5,
+    marginBottom: 5,
+    fontSize: 20,
+  },
+
+  description:{
+    fontFamily:'Lato',
+    justifyContent:'center',
+    alignContent:'center',
+    marginTop: 10,
+    textAlign: 'center',
+    fontSize: 15,
+  },
+});
